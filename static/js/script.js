65bdb9f2a30643e231df0dc49299fdc1427a26e8
Gustavo Peretti <gustavoperetti11@gmail.com>
2024-10-22 08:49:36 -0300

    Correção

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,12 @@
+window.addEventListener("scroll", () => {
+    var navbar2 = document.getElementById("navbar");
+    if (window.scrollY > 0) {
+        navbar2.style.background = "linear-gradient(135deg, rgb(222, 149, 120), rgb(230, 182, 119), rgb(222, 149, 120))";
+    } else {
+        navbar2.style.background = "transparent";
+    }
+});
+
 // Controle de cards em Recursos
 
 let recursoAtual = 0;
@@ -25,14 +34,4 @@ document.querySelector('#carrossel .tras').addEventListener('click', () => {
 
 document.querySelector('#carrossel .frente').addEventListener('click', () => {
     trocarRecurso(1)
-});
-
-
-window.addEventListener("scroll", () => {
-    var navbar2 = document.getElementById("navbar");
-    if (window.scrollY > 0) {
-        navbar2.style.background = "linear-gradient(135deg, rgb(222, 149, 120), rgb(230, 182, 119), rgb(222, 149, 120))";
-    } else {
-        navbar2.style.background = "transparent";
-    }
 });
