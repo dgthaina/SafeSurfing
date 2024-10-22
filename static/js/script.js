3e3e5d8d19778747368d9765909d6cf99b3eedfc
dgthaina <thainadagostini61@gmail.com>
2024-10-22 08:46:46 -0300

    atualizaçao da navbar colorida

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,20 @@
 window.addEventListener("scroll", () => {
     var navbar2 = document.getElementById("navbar");
     if (window.scrollY > 0) {
-        navbar2.style.background = "linear-gradient(135deg, rgb(222, 149, 120), rgb(230, 182, 119), rgb(222, 149, 120))";
+        navbar2.style.backgroundColor = "rgb(230, 182, 119)";
     } else {
-        navbar2.style.background = "transparent";
+        navbar2.style.backgroundColor = "transparent";
     }
 });
+
+var t = window.scrollY;
+
+window.onscroll = () => {
+    document.querySelectorAll('.reveal').forEach(e => {
+        if (e.getBoundingClientRect().top < window.innerHeight - 150) {
+            e.classList.add('active');
+        } else {
+            e.classList.remove('active');
+        }
+    });
+}
