db4911fa14bad2b29ba4d1feb2a0bb18b997145d
GustavoPeretti <gustavoperetti11@gmail.com>
2024-11-05 07:45:58 -0300

    Atualização do modal

--- static/js/script.js
+++ static/js/script.js
@@ -56,9 +56,15 @@ document.querySelector('#carrossel .frente').addEventListener('click', () => {
 
 const modal = document.querySelector('#modal');
 
+modal.querySelector('button').addEventListener('click', () => {
+    fecharModal();
+});
+
 function abrirModal(titulo, texto) {
     modal.style.visibility = 'visible';
     modal.style.opacity = '1';
+    modal.querySelector('.titulo').innerText = titulo;
+    modal.querySelector('.texto').innerText = titulo;
 }
 
 function fecharModal() {
