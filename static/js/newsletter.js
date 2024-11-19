532ab0906f739074822ffcecf0a7be74b094e230
GustavoPeretti <gustavoperetti11@gmail.com>
2024-11-19 17:55:19 -0300

    Campos da newsletter

--- static/js/newsletter.js
+++ static/js/newsletter.js
@@ -1,3 +1,11 @@
 document.querySelector('#nav-newsletter .voltar').addEventListener('click', () => {
     window.location.href = 'newsletter-menu.html';
 });
+
+document.querySelector('#e-mail .itens .imagem button').addEventListener('click', () => {
+    document.querySelector('#e-mail .itens .imagem input').click();
+});
+
+document.querySelector('#e-mail .itens .imagem input').addEventListener('change', () => {
+    document.querySelector('#e-mail .itens .imagem img').src = window.URL.createObjectURL(document.querySelector('#e-mail .itens .imagem input').files[0]);
+});
