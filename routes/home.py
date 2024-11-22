3ea65cfc789300b451145ece89a5a6a47c001977
GustavoPeretti <gustavoperetti11@gmail.com>
2024-11-21 22:12:00 -0300

    Inscrição

--- routes/home.py
+++ routes/home.py
@@ -1,7 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+from ..database.db import db
+import uuid
+import re
 
 home = Blueprint('home', __name__)
 
 @home.route('/')
 def home_handler():
     return render_template('index.html')
+
+@home.route('/newsletter', methods=['POST'])
+def cadastrar_email():
+    dados = request.json
+
+    if not 'e-mail' in dados:
+        return jsonify({'ok': False, 'mensagem': 'Parâmetro "e-mail" não especificado.'}), 400
+
+    padrao_email = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+
+    if not re.match(padrao_email, dados['e-mail']):
+        return jsonify({'ok': False, 'mensagem': 'E-mail inválido.'}), 400
+
+    try:
+        db.query('INSERT INTO inscritos VALUES (%s, %s);', dados['e-mail'], uuid.uuid4())
+    except:
+        return jsonify({'ok': False, 'mensagem': 'Houve um erro ao tentar realizar a inscrição.'}), 500
+    
+    return jsonify({'ok': True, 'mensagem': 'E-mail inscrito.'}), 201
--- static/js/footer.js
+++ static/js/footer.js
@@ -1,4 +1,4 @@
-document.querySelector('footer .esquerda button').addEventListener('click', () => {
+document.querySelector('footer .esquerda button').addEventListener('click', async () => {
     let email  = document.querySelector('footer .esquerda input').value;
 
     let emailExpression = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
@@ -8,8 +8,21 @@ document.querySelector('footer .esquerda button').addEventListener('click', () =
         return;
     }
 
-    if (false) {
-        abrirStatusModal(false, 'Não foi possível cadastrar o e-mail. Tente mais tarde.');
+    let resposta = await fetch('/newsletter', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'e-mail': email,
+        })
+    });
+
+    let respostaJSON = await resposta.json();
+
+    if (!respostaJSON.ok) {
+        abrirStatusModal(false, respostaJSON.mensagem);
         return;
     }
 
